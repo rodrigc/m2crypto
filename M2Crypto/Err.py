e26ea0bfa4cd583d789bde04dead4f38307bbdce
Matěj Cepl <mcepl@cepl.eu>
2016-05-13 17:05:05 +0200

    M2Crypto/Err.py: raise string error messages, not bytes

--- M2Crypto/Err.py
+++ M2Crypto/Err.py
@@ -4,30 +4,38 @@ from __future__ import absolute_import
 
 Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
 
-from M2Crypto import BIO, m2
+from M2Crypto import BIO, m2, util
+
 
 def get_error():
     err = BIO.MemoryBuffer()
     m2.err_print_errors(err.bio_ptr())
-    return err.getvalue()
+    return util.py3str(err.read())
+
 
 def get_error_code():
     return m2.err_get_error()
 
+
 def peek_error_code():
     return m2.err_peek_error()
+
 
 def get_error_lib(err):
     return m2.err_lib_error_string(err)
 
+
 def get_error_func(err):
     return m2.err_func_error_string(err)
+
 
 def get_error_reason(err):
     return m2.err_reason_error_string(err)
 
+
 def get_x509_verify_error(err):
     return m2.x509_get_verify_error(err)
+
 
 class SSLError(Exception):
     def __init__(self, err, client_addr):
@@ -35,7 +43,7 @@ class SSLError(Exception):
         self.client_addr = client_addr
 
     def __str__(self):
-        if (isinstance(self.client_addr, unicode)):
+        if (isinstance(self.client_addr, str)):
             s = self.client_addr.encode('utf8')
         else:
             s = self.client_addr
